6b1e5a258bb1eda14d16c532f73f67aea40b8965
Ario Dev <88058685+ArioDeveloper@users.noreply.github.com>
2022-08-15 17:25:05 +0430

    Update index.py

--- index.py
+++ index.py
@@ -8,18 +8,7 @@ from googlesearch import search
 
 app = Flask(__name__)
 
-def upload(a,g,l,f):
-    try:
-        bot = Bot(a)
-        
-        f=open(g+"."+f, "wb")
-        f.write(requests.get(l).content)
-        f.close()
-        bot.sendPhoto(g,g+"."+f,caption=l)
-        bot.sendDocument(g,g+"."+f,caption=l)
-        return "ok"
-    except Exception as e:
-        return e
+
 
 
 @app.route('/')
@@ -57,25 +46,20 @@ def file(path):
  
 
 
-@app.route('/contact')
-def contact():
-    return 'Contact Page Route'
+@app.route('/s' , methods=['GET'])
+def search():
+        q= request.args.get('q')
+        r=requests.get(f"https://boiling-mountain-37861.herokuapp.com/s?q={q}")
+        return r.text
+
+@app.route('/d' , methods=['GET'])
+def d():
+        r=requests.get(f"https://boiling-mountain-37861.herokuapp.com/d")
+        return r.text
+@app.route('/q' , methods=['GET'])
+def q():
+        q= request.args.get('q')
+        r=requests.get(f"https://boiling-mountain-37861.herokuapp.com/q?q={q}")
+        return r.text
 
 
-@app.route('/api' , methods=['GET'])
-def api():
-    t="pass erroe"
-    g = request.args.get('g')
-    a = request.args.get('a')
-    l = request.args.get('l')
-    p = request.args.get('p')
-    f = request.args.get('f')
-    if p == "zxcvbnm":
-       t=upload (a,g,l,f)
-    return str(t)
-@app.route('/search' , methods=['GET'])
-def searching():
-    list=search(request.args.get('q'),num_results=30)
-    return jsonify(list)
-    
-   
